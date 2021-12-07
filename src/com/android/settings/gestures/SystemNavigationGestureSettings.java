6bf40c97438d17a4f9f7bb7b8b5b8b341d72455a
jhonboy121 <alfredmathew05@gmail.com>
2021-12-07 17:15:29 +0800

    Settings: Squashed navigation/gestures patches
    Settings: allow hiding navbar [2/2]
    
    * simplified some existing code while at it
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    
    Settings: allow changing the length of gesture navbar [2/2]
    
    * forward ported from 11, but incorporated fullscreen gesture settings as a dependency so that this preference will be disabled when pill is hidden.
    
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    Settings: Improve gesture bar length [2/2]
    
    Signed-off-by: cjh1249131356 <cjh1249131356@gmail.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    
    Co-authored-by: cjh1249131356 <cjh1249131356@gmail.com>
    Co-authored-by: 703joko <703joko@gmail.com>
    Signed-off-by: str-br <alexfinhart@gmail.com>
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: I159cd1985d2596f9da098b248f6a86b5dc7606d5
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/settings/gestures/SystemNavigationGestureSettings.java
+++ src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -77,6 +77,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
             "pref_system_navigation_suggestion_complete";
 
     private static final String KEY_SHOW_A11Y_TUTORIAL_DIALOG = "show_a11y_tutorial_dialog_bool";
+    private static final String FULLSCREEN_GESTURE_OVERLAY_PKG = "com.custom.overlay.systemui.navbar.gestural";
 
     static final String LAUNCHER_PACKAGE_NAME = "com.google.android.apps.nexuslauncher";
 
@@ -310,7 +311,6 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
         }
     }
 
-    @VisibleForTesting
     static void setCurrentSystemNavigationMode(IOverlayManager overlayManager, String key) {
         String overlayPackage = NAV_BAR_MODE_GESTURAL_OVERLAY;
         switch (key) {
