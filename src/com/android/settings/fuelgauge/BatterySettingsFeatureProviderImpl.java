1a5def46801bb541029e92590af6ffa17bad016a
Michael Bestas <mkbestas@gmail.com>
2024-03-15 16:10:49 +0200

    Make battery info configurable and enable it by default
    Change-Id: If38db229e420e9f7c0ac26abb42a2a309b2f87c2
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/com/android/settings/fuelgauge/BatterySettingsFeatureProviderImpl.java
+++ src/com/android/settings/fuelgauge/BatterySettingsFeatureProviderImpl.java
@@ -23,6 +23,7 @@ import androidx.annotation.Nullable;
 
 import com.android.settings.fuelgauge.batterytip.BatteryTipPolicy;
 import com.android.settings.fuelgauge.batterytip.detectors.LowBatteryDetector;
+import com.android.settings.R;
 import com.android.settings.fuelgauge.batterytip.tips.BatteryTip;
 
 import java.util.List;
@@ -42,7 +43,7 @@ public class BatterySettingsFeatureProviderImpl implements BatterySettingsFeatur
 
     @Override
     public boolean isBatteryInfoEnabled(Context context) {
-        return false;
+        return context.getResources().getBoolean(R.bool.config_show_battery_info);
     }
 
     @Override
